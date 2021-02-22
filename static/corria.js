92009c7d1dc3c1f35675c40bb9f11e4c1553274f
avysbf <66377593+avysbf@users.noreply.github.com>
2021-02-22 11:25:46 -0300

    Add files via upload

--- static/corria.js
+++ static/corria.js
@@ -1,11 +1,11 @@
 var app = document.getElementById('typer');
 
 var typedio = new Typewriter(app, {
-    cursor: "|"
+    cursor: "<i>|</i>"
 });
 
 let paragrafo = 
-'Corria uma tarde enformigada de cajueiros na ponta do horizonte chovia baba de Iemanjá, que tava que tava danada! Taludinha, bebendo açude pelos mundaréus. E se foi rachando arco-íris, chovendo colorido nas estripulias de Toró fazendo chuva nuns batuques.';
+'<b>Corria uma tarde enformigada de cajueiros na ponta do horizonte chovia baba de <i>Iemanjá</i>, que tava que tava danada! Taludinha, bebendo açude pelos mundaréus. E se foi rachando arco-íris, chovendo colorido nas estripulias de Toró fazendo chuva nuns batuques.</b>';
 
 typedio.typeString(paragrafo).start();
 
